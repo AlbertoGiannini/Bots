f71f6c3f73237268127b18729e5616eac021d1ee
Alberto Giannini <74364239+AlbertoGiannini@users.noreply.github.com>
2023-10-16 12:26:13 -0300

    bot

--- jogovelha.py
+++ jogovelha.py
@@ -1,16 +1,37 @@
 import discord
-from discord.ext import commands
 from discord.ext.commands import bot
-from jogovelhaV2 import players
+from discord.ext import commands
+from random import randint
+from discord import FFmpegPCMAudio
+import time
+
+intents = discord.Intents.all()
+intents.members = True
+intents.message_content = True
+intents.typing = False
+intents.presences = False
+intents.voice_states = True
+ 
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+client = discord.Client(intents=intents)
 
 @bot.command()
-async def join(ctx):
-    if ctx.channel.name != 'jogo-velha':
+async def jogovelha(ctx):
+  #try:
+  channel = await ctx.guild.create_text_channel('jogo-velha')
+  #except:
+    #await ctx.send('{} Oops! Estou sem as permissões do servidor!'.format(ctx.author.mention))
+    
+  await channel.send('{} Digite ?join para entrar. É preciso de 2 jogadores para iniciar'.format(ctx.author.mention))
+  players = []
+  @bot.command()
+  async def join(ctx):
+    if ctx.channel.name != channel.name:
       await ctx.send('Oops! Chat errado')
+    elif len(players) == 2:
+      await ctx.send('Já existem 2 jogadores')
     else:
-      if len(players) == 2:
-        await ctx.send('Já existem 2 jogadores')
-      else:
         j = len(players)
         for x in players:
           if ctx.author.id == x:
@@ -25,34 +46,31 @@ async def join(ctx):
           print('J ', j)   
 
         print(players)
-        
-@bot.command()
-async def play(ctx):
-    if ctx.channel.name != 'jogo-velha':
+  @bot.command()
+  async def play(ctx):
+    if ctx.channel.name != channel.name:
       await ctx.send('Oops! Chat errado')
+    elif len(players) < 2:
+      await ctx.send('{} É preciso ter 2 jogadores para iniciar!'.format(ctx.author.mention))
+    else: 
+      if ctx.author.id !=  players[0] and ctx.author.id != players[1]:
+          print('ID ', ctx.author.id)
+          await ctx.send('{} não se mete corno'.format(ctx.author.mention))
 
-    else:
-      if len(players) < 2:
-        await ctx.send('{} É preciso ter 2 jogadores para iniciar!'.format(ctx.author.mention))
       else: 
-        if ctx.author.id !=  players[0] and ctx.author.id != players[1]:
-            print('ID ', ctx.author.id)
-            await ctx.send('{} não se mete'.format(ctx.author.mention))
-
-        else: 
           matriz = []
           j = 0
           while j < 9:
             matriz.append('-')
             j += 1
 
-            async def tabela(ctx):
-              p = 0
-              mensagem = ''
-              while p < 9:
-                mensagem += '\n{}   |   {}   |   {}'.format(matriz[p], matriz[p+1], matriz[p+2])
-                p += 3
-              await ctx.send(mensagem)
+          async def tabela(ctx):
+            p = 0
+            mensagem = ''
+            while p < 9:
+              mensagem += '\n{}   |   {}   |   {}'.format(matriz[p], matriz[p+1], matriz[p+2])
+              p += 3
+            await ctx.send(mensagem)
 
 
           ##message = await client.wait_for('message', check=lambda message: message.author == ctx.author)
@@ -61,32 +79,29 @@ async def play(ctx):
           async def jogo(ctx):
             msg = await bot.wait_for('message', check=lambda message: message.channel == ctx.channel)
 
-            if msg.author.id != players[0] and msg.author.id != players[1]:
-              await ctx.send('{} Você não está jogando'.format(msg.author.mention))
-            else:
+            if msg.author.id == players[0] or msg.author.id == players[1]:
               nonlocal win
               while msg != 'sair' or win == 9:
-              
                 try:
                   num = int(msg.content) -1
                 except:
-                  await ctx.send('{} Digite um número entre 1 e 9!'.format(ctx.author.mention))
+                  await ctx.send('Digite um número entre 1 e 9!')
                   await jogo(ctx)
 
                 if num > 9 or num < 0:
-                  await ctx.send('{} Digite um número entre 1 e 9!'.format(ctx.author.mention))
-      
-                else:
+                  await ctx.send('Digite um número entre 1 e 9!')
+                
+                elif msg.author.id == players[0] or msg.author.id == players[1]:
                   jogada = '' 
                   if msg.author.id == players[0]:
                     jogada = 'X'
                   else:
                     jogada = 'O'
-                  
-                  if matriz[num] ==  'X' or matriz[num] ==  'O':
-                    await ctx.send('{} Posição ocupada!'.format(ctx.author.mention))
-                    await tabela(ctx)
 
+                  if matriz[num] != '-':
+
+                    await ctx.send('{} Posição ocupada!'.format(ctx.author.mention)) 
+                  
                   else:
                     matriz[num] = jogada
                     await tabela(ctx)
@@ -111,16 +126,30 @@ async def play(ctx):
                       if matriz[0] == t and  matriz[4] == t and matriz[8]  == t:
                         await ctx.send('{} Venceu!'.format(player.mention))
                         exit()
-                      if matriz[2] == t and  matriz[4] == t and matriz[6]  == t:
+                      elif matriz[2] == t and  matriz[4] == t and matriz[6]  == t:
                         await ctx.send('{} Venceu!'.format(player.mention))
                         exit()    
-
+                      elif win == 8:
+                        await ctx.send('Deu velha!')
+                        exit() 
                     
                     if win >= 4:
                       await testvencedor(ctx, matriz[num])      
-                    win = win + 1         
+                    win = win + 1  
+                else:
+                  await ctx.send('{} Você não está jogando'.format(msg.author.mention))
+                  await tabela(ctx)       
 
                 msg = await bot.wait_for('message', check=lambda message: message.channel == ctx.channel)
                 print('WIN:', win)
           await jogo(ctx)
-          
+          await channel.delete()
+
+
+@bot.event
+async def on_message(message):
+  if message.author == bot.user:
+    return
+  await bot.process_commands(message)
+
+bot.run('TOKEN')
